5dc193f9f6266623b5ff2bf8bcf5d26ad76c685f
Will Langhart <theholotrout@gmail.com>
2025-01-14 12:51:16 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "postgresql://user:password@localhost/rag_chatbot")
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "postgresql://holotrout:9455@localhost/rag_chatbot")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
     PINECONE_API_KEY = os.getenv("PINECONE_API_KEY")
